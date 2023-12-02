7f0e18ddb3af8dbe512e1de8272a9fd4af282666
mannaja <manman7852078520@gmail.com>
2023-12-03 04:00:09 +0700

    Update lab6_1

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -4,12 +4,14 @@ using namespace std;
 int main(){
     int num=1, Odd=0, Even=0;
 
-    while (num != 0){
+    while (true){
     cout << "Enter an integer: ";
     cin >> num;
+
+    if (num == 0){ break; }
     
-    Odd =  (num % 2 == 1) ? Odd + 1 : Odd;
     Even = (num % 2 == 0 && num != 0) ? Even + 1 : Even;
+    Odd =  (num % 2 == 1) ? Odd + 1 : Odd;
     }
 
     cout << "#Even numbers = "<< Even <<"\n";
